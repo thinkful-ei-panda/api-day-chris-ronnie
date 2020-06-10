27ac78c7e3a1aa944cce8b6f8f66f36a2a56bd30
Ronnie Holguin <ronnieholguin@gmail.com>
2020-06-10 09:16:17 -0700

    #3

---
+++ src/api.js
@@ -0,0 +1,10 @@
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ronnie'
+
+function getItems(){
+    return Promise.resolve('A succesful response!');
+};
+
+export default {
+    getItems
+};
