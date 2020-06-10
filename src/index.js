121230f6c271ee8045e1056c61a56996437be9bd
Ronnie Holguin <ronnieholguin@gmail.com>
2020-06-10 14:00:35 -0700

    final solution

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import $ from 'jquery';
-import api from './api.js';
+
 import 'normalize.css';
 import './index.css';
+
+import api from './api';
+import store from './store';
 import shoppingList from './shopping-list';
 
 const main = function () {
   api.getItems()
-  .then(response => response.json())
-  .then(response => console.log(response));
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
 
-$(main); 
+$(main);
